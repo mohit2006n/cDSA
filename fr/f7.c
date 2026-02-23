46af71427c2ff1813ab8e3cae967656bb2677604
Mohit <mohit@Mohit.local>
2026-02-23 15:12:19 +0530

    code

--- fr/f7.c
+++ fr/f7.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-    char st[20] = "Program";
-    for(int i=0; i<7; i++){
-        printf("%c\n", st[i]);
-    }
+    // char email[50];
+    // printf("Enter you email: ");
+    // scanf("%s",email);
+    // if(strchr(email,'@')&& strchr(email,'.')){
+    //     printf("Vaild email format.\n");
+    // }else{
+    //     printf("Invalid email format.\n");
+    // }
+
+    char firstName[30], lastName[30], fullName[100];
+    printf("Enter first Name : ");
+    scanf("%s", firstName);
+    printf("Enter last Name : ");
+    scanf("%s", lastName);
+    strcpy(fullName, firstName);
+    strcat(fullName, " ");
+    strcat(fullName, lastName);
+    printf("Full name: %s\n", fullName);
     return 0;
 }
